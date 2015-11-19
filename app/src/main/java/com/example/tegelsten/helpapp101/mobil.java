f11cbcd264d38284faf5129a6033789d165b09c9
Tegelsten <etagebrand@hotmail.com>
2015-11-19 13:11:26 +0100

    Nya aktivitetss

--- app/src/main/java/com/example/tegelsten/helpapp101/mobil.java
+++ app/src/main/java/com/example/tegelsten/helpapp101/mobil.java
@@ -1,26 +1,22 @@
 package com.example.tegelsten.helpapp101;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.example.tegelsten.helpapp101.R;
-
-public class MainActivity extends AppCompatActivity {
+public class mobil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mobil);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_mobil, menu);
         return true;
     }
 
@@ -37,30 +33,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void kategorier(View view) {
-        Intent startNewActivity = new Intent(this,kategorier.class);
-
-        startActivity(startNewActivity);
-
-    }
-    public void datorkomponenter(View view) {
-        Intent startNewActivity = new Intent(this,datorkomponenter.class);
-
-        startActivity(startNewActivity);
-
-    }
-    public void android(View view) {
-        Intent startNewActivity = new Intent(this, mobil.class);
-
-        startActivity(startNewActivity);
-
-    }
-    public void videos(View view) {
-        Intent startNewActivity = new Intent(this,videos.class);
-
-        startActivity(startNewActivity);
-
     }
 }
